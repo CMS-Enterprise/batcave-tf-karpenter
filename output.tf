8f79c58c77e462482c18a986bbe85ef543080ff6
Arun Sanna <arunchowdary.sanna@cms.hhs.gov>
2022-04-13 18:36:16 -0700

    updated code

--- output.tf
+++ output.tf
@@ -4,7 +4,6 @@ output "karpenter_iam" {
 
 output "hr_manifest" {
   description = "The rendered manifest of the release as JSON"
-  value = helm_release.karpenter
+  value = helm_release.autoscaler
   sensitive = true
 }
-
